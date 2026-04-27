53626e435004201fde80810b918d6fcbe3298747
gchuf <gasper.cefarin@gmail.com>
2026-04-27 15:12:47 +0200

    implement bypassRBAC cache

--- artemis-server/src/main/java/org/apache/activemq/artemis/core/server/management/ArtemisMBeanServerGuard.java
+++ artemis-server/src/main/java/org/apache/activemq/artemis/core/server/management/ArtemisMBeanServerGuard.java
@@ -36,15 +36,19 @@ import java.io.IOException;
 import java.lang.reflect.Method;
 import java.security.Principal;
 import java.util.List;
+import java.util.Map;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ArtemisMBeanServerGuard implements GuardInvocationHandler {
 
    private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
    private JMXAccessControlList jmxAccessControlList = JMXAccessControlList.createDefaultList();
+
+   private final Map<ObjectName, Boolean> bypassRBACCache = new ConcurrentHashMap<>();
 
    public void init() {
       ArtemisMBeanServerBuilder.setGuard(this);
@@ -125,7 +129,7 @@ public class ArtemisMBeanServerGuard implements GuardInvocationHandler {
    }
 
    private boolean canBypassRBAC(ObjectName objectName) {
-      return jmxAccessControlList.isInAllowList(objectName);
+      return bypassRBACCache.computeIfAbsent(objectName, name -> jmxAccessControlList.isInAllowList(name));
    }
 
    @Override
@@ -162,6 +166,9 @@ public class ArtemisMBeanServerGuard implements GuardInvocationHandler {
       }
       Set<String> currentUserRoles = getCurrentUserRoles();
 
+      if (currentUserRoles.isEmpty()) {
+         return false;
+      }
       boolean authorized = authorizeUserForMethod(objectName, operationName, currentUserRoles);
 
       if (authorized) {
