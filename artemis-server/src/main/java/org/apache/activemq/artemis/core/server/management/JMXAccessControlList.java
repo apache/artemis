3c542dd072b0a20f048f3353ea4988d787ebd5e5
gchuf <gasper.cefarin@gmail.com>
2026-04-27 15:21:22 +0200

    add keyPropertyList cache

--- artemis-server/src/main/java/org/apache/activemq/artemis/core/server/management/JMXAccessControlList.java
+++ artemis-server/src/main/java/org/apache/activemq/artemis/core/server/management/JMXAccessControlList.java
@@ -50,6 +50,14 @@ public class JMXAccessControlList {
       return key2.length() - key1.length();
    };
 
+   private final Map<String, Map<String, String>> keyPropertyCache =
+      Collections.synchronizedMap(new LinkedHashMap<String, Map<String, String>>(128, 0.75f, true) {
+         @Override
+         protected boolean removeEldestEntry(Map.Entry<String, Map<String, String>> eldest) {
+               return size() > 5000;
+         }
+      });
+
    public void addToAllowList(String domain, String key) {
       TreeMap<String, Access> domainMap = new TreeMap<>(keyComparator);
       domainMap = allowList.putIfAbsent(domain, domainMap);
@@ -88,7 +96,14 @@ public class JMXAccessControlList {
       TreeMap<String, Access> domainMap = domainAccess.get(objectName.getDomain());
 
       if (domainMap != null) {
-         Map<String, String> keyPropertyList = objectName.getKeyPropertyList();
+
+         String cacheKey = objectName.getCanonicalName();
+         Map<String, String> keyPropertyList = keyPropertyCache.get(cacheKey);
+         if (keyPropertyList == null) {
+            keyPropertyList = objectName.getKeyPropertyList();
+            keyPropertyCache.put(cacheKey, keyPropertyList);
+         }
+
          for (Map.Entry<String, String> keyEntry : keyPropertyList.entrySet()) {
             String prefixFilter = keyEntry.getKey() + "=";
             String key = normalizeKey(keyEntry.getKey() + "=" + keyEntry.getValue());
