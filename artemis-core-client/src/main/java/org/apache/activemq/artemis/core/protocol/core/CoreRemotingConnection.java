clebertsuconic/activemq-artemis

ARTEMIS-5573 and ARTEMIS-5975 Improve AMQP Size estimation
- making it immutable to avoid races after updates like we had in the past
- avoiding scanning after reloading by persisting extra data on storage

Co-authored-by: Robbie Gemmell <robbie@apache.org>

artemis-core-client/src/main/java/org/apache/activemq/artemis/core/protocol/core/CoreRemotingConnection.java
@@ -81,6 +81,11 @@ public interface CoreRemotingConnection extends RemotingConnection {
       return version < PacketImpl.ARTEMIS_2_28_0_VERSION;
    }
 
+   default boolean isBeforeEmbedWireVersion2() {
+      int version = getChannelVersion();
+      return version < PacketImpl.ARTEMIS_2_56_0_VERSION;
+   }
+
    /**
     * Sets the client protocol used on the communication. This will determine if the client has support for certain
     * packet types

apache/artemis

artemis-core-client/src/main/java/org/apache/activemq/artemis/core/protocol/core/CoreRemotingConnection.java
@@ -81,6 +81,11 @@ public interface CoreRemotingConnection extends RemotingConnection {
       return version < PacketImpl.ARTEMIS_2_28_0_VERSION;
    }
 
+   default boolean isBeforeEmbedWireVersion2() {
+      int version = getChannelVersion();
+      return version < PacketImpl.ARTEMIS_2_58_0_VERSION;
+   }
+
    /**
     * Sets the client protocol used on the communication. This will determine if the client has support for certain
     * packet types